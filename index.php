b3bde375d17939052e5fa5e757cb4b53cf6b44db
pedrohmonteiro <pedrohenriquemon@gmail.com>
2020-10-14 21:41:26 -0300

    front

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+include("nav.php");
+$page_css[] = "index.css";
+?>
+
+<div>
+
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/index.css">
+
+        <title>Hello, world!</title>
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="select-box">
+                <div class="options-container">
+                    <div class="option">
+                        <input type="radio" class="radio" id="exmample" name="category" />
+                        <label for="exmample">exmample</label>
+                    </div>
+                </div>
+
+                <div class="selected">
+                    Selecione a especialidade
+                </div>
+
+                <div class="search-box">
+                    <input type="text" placeholder="" />
+                </div>
+
+                <script src="main.js"></script>
+            </div>
+        </div>
+</div>
+</div>
+
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+</body>
+
+</div>
